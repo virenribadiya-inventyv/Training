6ad9fbe337a9da6b1a9936a97b9f858eae865b53
virenribadiyainventyv <virenribadiyainventyv@gmail.com>
2023-12-10 16:38:11 +0530

    pattern code upload

---
+++ LB/Pattern04.cpp
@@ -0,0 +1,26 @@
+// 1! -3! 5! -7! 9! ...... n!
+// 1 -6 120 -5040 362880
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,fact=1;
+    cout<<"Enter n:";
+    cin>>n;
+
+    for(int i=1;i<=n+(n-1);i=i+2){
+        for(int j=1;j<=i;j++){
+            fact = fact*j;
+        }
+        cout<<fact<<" ";
+        if(fact>0){
+            fact = -1;
+        }
+        else{
+            fact = 1;
+        } 
+    }
+    cout<<endl;
+    return 0;
+}
